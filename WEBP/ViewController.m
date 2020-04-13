2bc590ce76527db6356055af77d0e3474f7edc31
yz <591xt@591xt.xyz>
2020-04-13 23:10:56 +0800

    yz

--- WEBP/ViewController.m
+++ WEBP/ViewController.m
@@ -1,11 +1,7 @@
-//
-//  ViewController.m
-//  WEBP
-//
-//  Created by YZ on 2018/6/4.
-//  Copyright © 2018年 TZ. All rights reserved.
-//
-#import "UIImage+WebP.h"
+
+
+
+#import "UIImage+YZWebp.h"
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,13 +12,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //模拟器没效果，直接上真机才行
+
     self.view.backgroundColor=[UIColor whiteColor];
     UIImageView* webp=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    NSString* str=[[NSBundle mainBundle] pathForResource:@"a.webp" ofType:nil];
-    webp.image=[UIImage imageWithWebP:str];
-    webp.center=self.view.center;
+    //后缀为小写的时候可以省略
+    webp.image=[UIImage imageWithWebP:@"a"];
+    webp.backgroundColor=[UIColor redColor];
+    webp.center=CGPointMake(self.view.center.x, self.view.center.y*0.5);
     [self.view addSubview:webp];
+    
+    UIImageView* webp1=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    webp1.image=[UIImage imageWithWebP:@"a.webp"];
+    webp1.backgroundColor=[UIColor greenColor];
+    webp1.center=self.view.center;
+    [self.view addSubview:webp1];
+    
+    //名字大小写不对识别不了
+    UIImageView* webp2=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    webp2.image=[UIImage imageWithWebP:@"a.WEBP"];
+    webp2.center=CGPointMake(self.view.center.x, self.view.center.y*1.5);
+    webp2.backgroundColor=[UIColor blueColor];
+    [self.view addSubview:webp2];
+    
 }
 
 
